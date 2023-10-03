129b28203ef2d4bfad2cdbe7a9acec5b3e55422a
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2023-10-03 22:27:43 +0600

    done

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8000/api/v3/',
+    baseUrl: 'https://portfolio-server-pied-one.vercel.app/api/v3/',
   }),
   tagTypes: [],
   endpoints: (builder) => ({}),
